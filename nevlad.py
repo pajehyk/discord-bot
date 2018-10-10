a0ff452dd6e2f378aa62d579e525cfedcf2f6b2b
pajehyk <pajehyk@gmail.com>
2018-10-10 15:23:21 +0900

    Update nevlad.py

--- nevlad.py
+++ nevlad.py
@@ -2,9 +2,19 @@ import discord;
 import asyncio;
 import requests;
 
-DISCORD_BOT_TOKEN = "";
+DISCORD_BOT_TOKEN = "NDk2NjY1MDA3ODc5MzU2NDM3.DpokeQ.Uh2R2prqKDNe80MpNusS7H4riWk";
 
 client = discord.Client();
+
+global myServer;
+myServer = myServer = client.get_server('359631684330848256');
+idChopChop = '323366007030677504';
+idChopper = '352411742766366721';
+idRoleHomo = '497930663802961921';
+idRoleAdmin = '359660189344858122';
+
+
+
 
 @client.event
 async def on_ready():
@@ -13,18 +23,27 @@ async def on_ready():
     print(client.user.id);
     print("-------");
 
+    global myServer;
     myServer = client.get_server('359631684330848256');
-    global homoRole;
+    global roleAdmin;
+    global roleHomo;
+    
     if myServer != None:
-        if '373405537095647235' in [y.id for y in myServer.roles]:
+        if idRoleHomo in [y.id for y in myServer.roles]:
             for y in myServer.roles:
-                if y.id == '373405537095647235':
-                    homoRole = y;
+                if y.id == idRoleHomo:
+                    roleHomo = y;
+        if idRoleAdmin in [y.id for y in myServer.roles]:
+            for y in myServer.roles:
+                if y.id == idRoleAdmin:
+                    roleAdmin = y;
     
 
 @client.event
 async def on_message(message):
+    
     if message.content.startswith('!test'):
+        print('[command]: test');
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
@@ -32,23 +51,37 @@ async def on_message(message):
                 counter += 1
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
+    if message.content.startswith('!sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
         
     if message.content.startswith('!hi'):
-        print("[command]: hi ");
-        await client.send_message(message.channel, 'asdasd');
-    if message.content.startswith('!role'):
-        print("[command]: role ");
-        await client.send_message(message.channel, message.author.roles[1].id);
-        print(message.author.roles[1].id);
+        print('[command]: hi ');
+        await client.send_message(message.channel, 'маму ебал');
+
+    #admin
+    if roleAdmin in message.author.roles:
+        if message.content.startswith('!deletechopper'):
+            tmp = await client.send_message(message.channel, 'deleting...');
+            for y in myServer.members:
+                if y.id == idChopper:
+                    userChopper = y;
+                if y.id == idChopChop:
+                    userChopChop = y;
+            
+            async for log in client.logs_from(message.channel, limit=100):
+                if (log.author.id == idChopper or log.author.id == idChopChop):
+                    await client.delete_message(log);
+
+            await client.edit_message(tmp, 'deleted kara armenin.');
+
+        
+        
 
 @client.event
 async def on_member_join(member):
-    print('member joined: ', member.nick, member.roles[0]);
-    await client.add_roles(member, homoRole);
-    print(member.roles);
+    print('member joined: ', member.name);
+    await client.add_roles(member, roleHomo);
     
 
 client.run(DISCORD_BOT_TOKEN);
